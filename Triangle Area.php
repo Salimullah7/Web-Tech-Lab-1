b0df2ac1b37e7c54b6b4ca7b47b0ee7f9b21e2f3
Salimullah7 <60336405+Salimullah7@users.noreply.github.com>
2020-02-10 10:44:58 +0600

    Add files via upload

---
+++ Triangle Area.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 20;  
+$y = 30;
+
+echo "the area of ractangle= " $x * $y;
+echo "Perimeter is=" 2*($x+$y);
+?>  
+
+</body>
+</html>
---
+++ Vat.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 100;  
+
+echo "VAT is= " $x * .15
+;
+
+?>  
+
+</body>
+</html>
